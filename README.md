d83e20503ed06ec76d2f2a6e48692d3f04468938
pajicadvance <pavle.pajic23@gmail.com>
2024-12-01 20:20:10 +0100

    Cleanup

---
+++ README.md
@@ -0,0 +1,3 @@
+## pajic's advent of code 2024 solutions
+
+they're probably not good but they're mine
--- src/main/java/day1/Day1.java
+++ src/main/java/day1/Day1.java
@@ -4,25 +4,24 @@ import it.unimi.dsi.fastutil.ints.IntArrayList;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 
 public class Day1 {
 
-    public static void main(String[] args) {
+    public static void run() {
         IntArrayList list1 = new IntArrayList();
         IntArrayList list2 = new IntArrayList();
 
-        Path input = Paths.get("src/main/resources/inputs/day1");
-        try (BufferedReader br = Files.newBufferedReader(input)) {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(Day1.class.getResource("/inputs/day1").toURI()))) {
             br.lines().forEach(line -> {
                 String[] split = line.split("\\s+");
                 list1.add(Integer.parseInt(split[0]));
                 list2.add(Integer.parseInt(split[1]));
             });
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace(System.err);
         }
 
@@ -41,7 +40,6 @@ public class Day1 {
             similarityScore += list1.getInt(i) * occurrences;
         }
 
-        System.out.println("Total distance: " + totalDistance);
-        System.out.println("Similarity score: " + similarityScore);
+        System.out.println("----- Day 1 -----" + "\nTotal distance: " + totalDistance + "\nSimilarity score: " + similarityScore);
     }
 }
